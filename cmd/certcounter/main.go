f09d34cb1450899a18c9155d0c061fbb5ccd8f32
ginokent <29125616+ginokent@users.noreply.github.com>
2022-10-12 02:44:04 +0900

    config.Load

--- cmd/certcounter/main.go
+++ cmd/certcounter/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"os"
 	"os/signal"
 	"syscall"
@@ -30,20 +31,32 @@ func main() {
 
 func Main(ctx context.Context, l *rec.Logger) error {
 	shutdownChan := make(chan os.Signal, 1)
-	signal.Notify(shutdownChan, syscall.SIGINT, syscall.SIGTERM)
+	signal.Notify(shutdownChan, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM)
 	ctx = contextz.WithSignalChannel(ctx, shutdownChan)
 
-	config.Load(l)
+	rollback, err := config.Load(l)
+	defer rollback()
+
+	// NOTE: If -h option, return nil
+	if errors.Is(err, flag.ErrHelp) {
+		return nil
+	}
+
+	// NOTE: display version info
 	l.Info("version info", rec.Object("version", map[string]string{"Version": config.Version(), "Revision": config.Revision(), "Branch": config.Branch(), "Timestamp": config.Timestamp(), "GoVersion": config.GoVersion()}))
+
+	// NOTE: If -v option, return nil
 	if config.SubcommandVersion() {
 		return nil
 	}
-	must.Must(config.Check())
+
+	// NOTE: If err != nil, panic(err)
+	must.Must(err)
 
 	l.F().Infof("main: 🔆 start %s (pid:%d)", consts.AppName, os.Getpid())
 	defer func() { l.F().Infof("main: 💤 shutdown %s (pid:%d)", consts.AppName, os.Getpid()) }()
 
-	shutdown, errCh := entrypoint.CertCounter(ctx, l)
+	shutdown, errChanCertCounter := entrypoint.CertCounter(ctx, l)
 	defer func() {
 		shutdown()
 		time.Sleep(1 * time.Millisecond) // NOTE: wait shutdown log
@@ -56,15 +69,20 @@ SignalLoop:
 			l.E().Info(ctx.Err())
 			break SignalLoop
 		case sig := <-shutdownChan:
+			l.F().Infof("main: catch the signal: %s", sig)
 			if sig == syscall.SIGHUP {
-				l.Info("load")
+				l.Info("main: reload config")
+				rollback, err := config.Load(l)
+				if err != nil {
+					l.Info("main: failed to load config. rollback")
+					rollback()
+				}
 				continue
 			}
-			l.F().Infof("main: catch the signal: %s", sig)
 			break SignalLoop
-		case err := <-errCh:
+		case err := <-errChanCertCounter:
 			if err != nil {
-				return errors.Errorf("entrypoint.StartGRPCGatewayAsync: %w", err)
+				return errors.Errorf("main: entrypoint.CertCounter: %w", err)
 			}
 		}
 	}
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,12 +2,14 @@ package config
 
 import (
 	"flag"
+	"os"
 	"sync"
 	"time"
 
 	"github.com/kunitsuinc/certcounter/pkg/errors"
 	"github.com/kunitsuinc/rec.go"
 	"github.com/kunitsuinc/util.go/env"
+	"github.com/kunitsuinc/util.go/pointer"
 )
 
 var ErrFlagOrEnvIsNotEnough = errors.New("config: flag or environment variable is not enough")
@@ -39,46 +41,55 @@ type Config struct {
 
 // nolint: gochecknoglobals
 var (
-	cfg   = &Config{}
-	cfgMu sync.Mutex
+	cfgBackup = &Config{}
+	cfg       = &Config{}
+	cfgMu     sync.Mutex
 )
 
-func Load(l *rec.Logger) {
+func Load(l *rec.Logger) (rollback func(), err error) {
 	cfgMu.Lock()
 	defer cfgMu.Unlock()
+	cfgBackup = pointer.Pointer(*cfg)
+
+	rollback = func() {
+		cfgMu.Lock()
+		defer cfgMu.Unlock()
+		cfg = pointer.Pointer(*cfgBackup)
+		l.Info("config: loaded", rec.Object("config", cfg))
+	}
 
 	var timeout int64
 
+	fs := flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
 	// version
-	flag.BoolVar(&subcommandVersion, "v", false, "Display version info")
-	flag.BoolVar(&subcommandVersion, "version", subcommandVersion || false, "Display version info")
+	fs.BoolVar(&subcommandVersion, "v", false, "Display version info")
+	fs.BoolVar(&subcommandVersion, "version", subcommandVersion || false, "Display version info")
 	// config
-	flag.BoolVar(&cfg.Debug, "debug", env.BoolOrDefault(DEBUG, false), "Debug")
-	flag.StringVar(&cfg.AppEnv, "appenv", env.StringOrDefault(APP_ENV, ""), "TODO")
-	flag.StringVar(&cfg.Addr, "addr", env.StringOrDefault(ADDR, "0.0.0.0"), "TODO")
-	flag.IntVar(&cfg.Port, "port", env.IntOrDefault(PORT, 8080), "TODO")
-	flag.StringVar(&cfg.GRPCEndpoint, "grpc-endpoint", env.StringOrDefault(ADDR, "0.0.0.0:9090"), "TODO")
-	flag.Int64Var(&timeout, "shutdown-timeout", env.Int64OrDefault(SHUTDOWN_TIMEOUT, 10), "TODO")
-	flag.StringVar(&cfg.AWSProfile, "aws-profile", env.StringOrDefault(AWS_PROFILE, ""), "TODO")
-	flag.StringVar(&cfg.GoogleCloudProject, "gcp-project", env.StringOrDefault(GOOGLE_CLOUD_PROJECT, ""), "TODO")
-	flag.StringVar(&cfg.SpanExporter, "span-exporter", env.StringOrDefault(SPAN_EXPORTER, "gcloud"), "TODO")
+	fs.BoolVar(&cfg.Debug, "debug", env.BoolOrDefault(DEBUG, false), "Debug")
+	fs.StringVar(&cfg.AppEnv, "appenv", env.StringOrDefault(APP_ENV, ""), "TODO")
+	fs.StringVar(&cfg.Addr, "addr", env.StringOrDefault(ADDR, "0.0.0.0"), "TODO")
+	fs.IntVar(&cfg.Port, "port", env.IntOrDefault(PORT, 8080), "TODO")
+	fs.StringVar(&cfg.GRPCEndpoint, "grpc-endpoint", env.StringOrDefault(ADDR, "0.0.0.0:9090"), "TODO")
+	fs.Int64Var(&timeout, "shutdown-timeout", env.Int64OrDefault(SHUTDOWN_TIMEOUT, 10), "TODO")
+	fs.StringVar(&cfg.AWSProfile, "aws-profile", env.StringOrDefault(AWS_PROFILE, ""), "TODO")
+	fs.StringVar(&cfg.GoogleCloudProject, "gcp-project", env.StringOrDefault(GOOGLE_CLOUD_PROJECT, ""), "TODO")
+	fs.StringVar(&cfg.SpanExporter, "span-exporter", env.StringOrDefault(SPAN_EXPORTER, "gcloud"), "TODO")
 	// parse
-	flag.Parse()
+	if err := fs.Parse(os.Args[1:]); err != nil {
+		return rollback, errors.Errorf("(*flag.FlagSet).Parse: %w", err)
+	}
 
 	cfg.ShutdownTimeout = time.Duration(timeout) * time.Second
 
 	l.Info("config: loaded", rec.Object("config", cfg))
-}
 
-// nolint: cyclop
-func Check() error {
 	switch {
 	case cfg.Addr == "":
-		return errors.Errorf("%s: %w", ADDR, ErrFlagOrEnvIsNotEnough)
+		return rollback, errors.Errorf("%s: %w", ADDR, ErrFlagOrEnvIsNotEnough)
 	case cfg.Port == 0:
-		return errors.Errorf("%s: %w", PORT, ErrFlagOrEnvIsNotEnough)
+		return rollback, errors.Errorf("%s: %w", PORT, ErrFlagOrEnvIsNotEnough)
 	case cfg.GRPCEndpoint == "":
-		return errors.Errorf("%s: %w", GRPC_ENDPOINT, ErrFlagOrEnvIsNotEnough)
+		return rollback, errors.Errorf("%s: %w", GRPC_ENDPOINT, ErrFlagOrEnvIsNotEnough)
 	}
 
 	switch {
@@ -87,10 +98,10 @@ func Check() error {
 	case cfg.GoogleCloudProject != "":
 		break
 	default:
-		return errors.Errorf("%s || %s: %w", AWS_PROFILE, GOOGLE_CLOUD_PROJECT, ErrFlagOrEnvIsNotEnough)
+		return rollback, errors.Errorf("%s || %s: %w", AWS_PROFILE, GOOGLE_CLOUD_PROJECT, ErrFlagOrEnvIsNotEnough)
 	}
 
-	return nil
+	return rollback, nil
 }
 
 func Debug() bool                    { return cfg.Debug }
